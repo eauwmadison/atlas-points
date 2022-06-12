8440bfa9e9595baa3e47276f6f71feca5f5c3083
eeshaan <me@eeshaan.life>
2022-06-11 23:16:37 -0500

    fix Firebase authentication

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,31 +1,39 @@
+import { cert, initializeApp, ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+import { Guild } from "discord.js";
+
+import serviceAccountKey from "../../serviceAccountKey.json";
+
+// initialize Firebase
+initializeApp({
+  credential: cert(serviceAccountKey as ServiceAccount)
+});
 const db = getFirestore();
 
-export async function registerGuildIfNotExists(guildId: string) {
-  console.log(`Attempting to register guild ${guildId} in database.`);
+export async function registerGuildIfNotExists(guild: Guild) {
+  console.log(`Attempting to register Guild ${guild.id} in database.`);
+
+  // const users = await guild.members
+  //   .fetch()
+  //   .then((members) => members.map((member) => member.user.id));
+
+  // console.log(users);
+
   try {
-    await db
-      .collection("guilds")
-      .doc(guildId)
-      .create({ users: {} },);
+    await db.collection("guilds").doc(guild.id).create({ users: {} });
+    console.log(`Registered Guild ${guild.id} in database.`);
   } catch (e) {
     console.log(e);
   }
 }
-
 
 export async function registerUserIfNotExists(guildId: string, userId: string) {
   try {
-    await db
-      .collection(`guilds/${guildId}/users`)
-      .doc(userId)
-      .create({
-        points: 0
-      });
+    await db.collection(`guilds/${guildId}/users`).doc(userId).create({
+      points: 0
+    });
   } catch (e) {
     console.log(e);
   }
 }
-
-
