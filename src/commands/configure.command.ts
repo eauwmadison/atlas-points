090372bcb2b4b594f20dfd3b006371bf064fabe3
eeshaan <me@eeshaan.life>
2022-06-22 10:18:40 -0500

    add permission boundary to `configure`

--- src/commands/configure.command.ts
+++ src/commands/configure.command.ts
@@ -1,8 +1,9 @@
 import { CommandInteraction, Client } from "discord.js";
-import /* displayErrorMessage */
-"../utils";
+
+import { checkPermissionRole, displayErrorMessage } from "../utils";
 
 import { Command } from "../command";
+import { getPermissionRoleName } from "../db/db";
 
 const Configure: Command = {
   name: "configure",
@@ -12,7 +13,8 @@ const Configure: Command = {
   options: [
     {
       name: "log",
-      description: "Configure the channel to output transaction logs to",
+      description:
+        'Configure the channel to output transaction logs to. No "channel" option will disable log output.',
       type: "SUB_COMMAND",
       options: [
         {
@@ -21,9 +23,43 @@ const Configure: Command = {
           type: "CHANNEL"
         }
       ]
+    },
+    {
+      name: "role",
+      description:
+        'Configure the role which has permission to modify points and settings. Default is "Instructor".',
+      type: "SUB_COMMAND",
+      options: [
+        {
+          name: "role",
+          description: "the role to give permission",
+          type: "ROLE",
+          required: true
+        }
+      ]
     }
   ],
   execute: async (_client: Client, interaction: CommandInteraction) => {
+    if (!interaction.guild) {
+      await displayErrorMessage(
+        interaction,
+        "Settings can only be configured in a server."
+      );
+      return;
+    }
+
+    const permitted = await checkPermissionRole(interaction);
+
+    if (!permitted) {
+      await displayErrorMessage(
+        interaction,
+        `Only members with the role "${await getPermissionRoleName(
+          interaction.guild.id
+        )}" can configure settings.`
+      );
+      return;
+    }
+
     await interaction.reply("Not yet configured.");
   }
 };
